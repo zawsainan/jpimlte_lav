23d4bb45462008a4fd328dd3b6b2193c03671fdb
Zawsainan <zawsainan@gmail.com>
2025-02-18 12:29:44 +0630

    Modified few pages

--- resources/views/pages/about-firm.blade.php
+++ resources/views/pages/about-firm.blade.php
@@ -1,34 +1,50 @@
 @extends("layout")
 @section("content")
-    <x-hero :image="asset('images/about/hero.png')" text="About Us"></x-hero>
-    <div class="container px-5 py-3 d-flex flex-column">
-        <section class="fade-in mb-3">
-            <h3 class="mb-4 fs-2">The Firm</h3>
-            <div class="row">
-                <div class="col-12 col-lg-6">
-                    <p class="mb-2">Japan Property Investment Management is a reputable real estate investment 
-                        firm based in  Singapore,  committed to delivering attractive returns through strategic  investments in  high-quality  real estate  assets  across Japan. Our team boasts over 50 years of combined experience in private banking, wealth 
-                        management, asset management, project financing, deal structuring, project and construction management, with a strong focus on the Asia Pacific region. We specialize in identifying premium investment  opportunities that align with our clients&#39; goals and aspirations. <br><br>
-                    The firm&#39;s investment strategy primarily targets boutique  hotels,  apartments, serviced residences, and other income-generating  properties situated in key urban  hubs,  with a particular emphasis on the dynamic markets of Tokyo and Osaka. By leveraging  our extensive market knowledge  and local  expertise,  we aim to maximize  value and  generate consistent, sustainable  returns for our investors through carefully curated property assets. A central  component of this strategy is  our strong  dedication  to  sustainability  and green  initiatives. The firm  seeks to create enduring value in Japan&#39;s dynamic real estate market, 
-                        benefiting both our investors and the environment.</p>
-                </div>
-                <img src="{{asset('images/about/sub-hero.png')}}" class="firm-story-img col-lg-6 d-none d-lg-block" alt="">
+<x-hero :image="asset('images/about/hero.png')" text="About Us"></x-hero>
+<div class="container px-5 py-3 d-flex flex-column">
+    <section class="fade-in mb-5">
+        <h3 class="mb-4 fs-2">The Firm</h3>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <p>Japan Property Investment Management is a reputable real estate investment firm based in Singapore, committed to
+                    delivering attractive returns through strategic investments in high-quality real estate assets across Japan. Our
+                    team members have collectively over 50 years of combined experience in banking, wealth and asset management, project
+                    financing, deal structuring, project construction management, with a strong focus in the Asia Pacific region. We
+                    specialize in identifying premium investment opportunities that align with our clients' goals and aspirations.</p>
+
+                <p>The firm&#39;s investment strategy primarily targets boutique hotels, apartments, serviced residences, and other
+                    income-generating properties situated in key urban hubs, with a particular emphasis on the dynamic markets of Tokyo
+                    and Osaka. By leveraging our extensive market knowledge and local expertise, we aim to maximize value and generate
+                    consistent, sustainable returns for our investors through carefully curated property assets.</p>
+
+                <p>A central component of this strategy is our strong dedication to sustainability and green initiatives. The firm
+                    seeks to create enduring value in Japan's dynamic real estate market, benefiting both our investors and the
+                    environment.</p>
             </div>
-        </section>
-        <div class="fade-in my-3">
-            <h3 class="h3 mb-4">Our Approach</h3>
-            <p>At Japan Property Investment Management, we emphasize a strong risk management approach by maintaining a sustainable capital structure that supports our investment strategies and effectively mitigates potential downside risks. Our deep understanding of the market  is complemented  by our local market expertise, where our extensive network and strong relationships provide us with access to valuable off-market opportunities. This enables us to identify unique investment prospects that others may not have access to. Furthermore, we cultivate  long-term strategic  partnerships built  on trust and collaboration, which have been instrumental in shaping our successful track record. <br><br>
-                We are also fortunate to benefit from the guidance of  experienced sponsor who  offer invaluable insights into the Japanese market and real estate development. The mentorship plays a key role in enhancing our strategic decision-making and strengthening our ability to capitalize on high-potential investment opportunities, 
-                ensuring continued success for our clients.
-            </p>
-            <div class="row">
-                <img src="{{asset('images/about/team1.png')}}" class="col-6">
-                <img src="{{asset('images/about/team2.png')}}" class="col-6">
-            </div>
-            <div class="row my-5 team-banner p-5 fw-bold" style="height: 30vh;">
-                    <h2 class="h2">Our Team</h2>
-                    <a class="nav-link ms-1" href="{{route('team')}}">MEET THE TEAM <i class="fa-regular fa-circle-right"></i></a>
-            </div>
-        </div>      
+            <img src="{{asset('images/about/sub-hero.png')}}" class="firm-story-img col-lg-6 d-none d-lg-block" alt="">
+        </div>
+    </section>
+    <div class="fade-in my-3">
+        <h3 class="h3 mb-4">Our Approach</h3>
+        <p>At Japan Property Investment Management, we emphasize a strong risk management approach by maintaining a sustainable
+            capital structure that supports our investment strategies and effectively mitigates potential downside risks. Our
+            deep understanding of the market, complemented by our local market expertise, extensive network and strong
+            relationships provide us with access to valuable off-market opportunities. This allows us to identify unique
+            investment prospects that others may not be privy to. We also cultivate long-term strategic partnerships built on
+            trust and collaboration, which have been instrumental in shaping our successful track record.</p>
+
+        <p>We are also fortunate to benefit from the guidance of an experienced sponsor, who offers invaluable insights into
+            the Japanese market and real estate development. This mentorship plays a key role in enhancing our strategic
+            decision-making and strengthening our ability to capitalize on high-potential investment opportunities, ensuring
+            continued success for our clients.</p>
+        <div class="row">
+            <img src="{{asset('images/about/team1.png')}}" class="col-6">
+            <img src="{{asset('images/about/team2.png')}}" class="col-6">
+        </div>
+        <div class="row my-5 team-banner p-5 fw-bold" style="height: 30vh;">
+            <h2 class="h2">Our Team</h2>
+            <a class="nav-link ms-1 fs-6" href="{{route('team')}}"><span class="btn btn-outline-dark">MEET THE TEAM <i class="fa-regular fa-circle-right"></i></span></a>
+        </div>
     </div>
+</div>
 @endsection
--- resources/views/pages/about-team.blade.php
+++ resources/views/pages/about-team.blade.php
@@ -1,52 +1,101 @@
 @extends("layout")
 @section("content")
-        <x-hero :image="asset('images/about/hero.png')" text="About Us"></x-hero>
+        <x-hero :image="asset('images/about/hero.png')" text="Our Team"></x-hero>
     <div class="container px-5 py-3">
-        <h3 class="h2 fw-bold">Mr. Jonathan Poh</h3>
-        <p>
-            Currently a Senior Partner at a reputable External Asset Management firm in Singapore, Jonathan brings with
+        <!--About Jonathan Poh-->
+        <h3 class="h3">Mr. Jonathan Poh</h3>
+        <p>Currently a Senior Partner at a reputable External Asset Management firm in Singapore, Jonathan brings with
             him over 25 years of invaluable banking and finance experience as the Executive Director with Deutsche Bank
             and UBS AG. Prior to his positions in Deutsche Bank and UBS AG, Jonathan&#39;s experiences also include
-            senior Client Advisor roles in Citibank and American Express Private Banking. <br><br>
+            senior Client Advisor roles in Citibank and American Express Private Banking.</p>
 
-            In his years as a personal and corporate financial and wealth advisor, Jonathan has formulated plans and
+        <p>In his years as a personal and corporate financial and wealth advisor, Jonathan has formulated plans and
             solutions for numerous Ultra-High Net-Worth clients in the Southeast Asia region. His commitment and
             dedication to his clients have helped him achieve several Service Excellence Awards throughout his career.
-            <br><br>
-
-            Jonathon&#39;s knowledge of financial asset management is demonstrated in his exceptional expertise and
-            proficiency in various aspects of the financial industry, both locally and internationally. <br><br>
-
         </p>
-        <p class="h4">Professional Qualifications:</p>
+
+        <p>Jonathan&#39;s knowledge of financial asset management is demonstrated in his exceptional expertise and
+            proficiency in various aspects of the financial industry, both locally and internationally.</p>
+
+        <h4 class="h4">Professional Qualifications:</h4>
         <ul>
-            <li>Bachelor of Business (Banking &amp; Finance - Honours),<br>
-                Nanyang Business School, Singapore
+            <li>
+                Bachelor of Business (Banking & Finance - Honours), Nanyang Business School, Singapore
             </li>
-            <li>Certified Financial Planner(CFP), Sinagpore</li>
+
+            <li>
+                Certified Financial Planner (CFP), Singapore
+            </li>
         </ul>
-        <h3 class="h2 fw-bold mt-5">Mr. Brandon Lye</h3>
+
+
+        <!--About Brandon Lye-->
+        <h3 class="h3 mt-5">Mr. Brandon Lye</h3>
         <p>Brandon has over 25 years of corporate, consulting and investment experience across Asia, specializing in
             investment, merger and acquisition and strategic planning. Brandon's professional experience includes a role
             as a Director at PricewaterhouseCoopers Singapore (PwC Singapore), where he led engagements in the Deals
             advisory practice. Brandon has gained deep experiences in deals structuring and investment in the real
-            estate, infrastructure, financial services and technology sectors. <br><br>
+            estate, infrastructure, financial services and technology sectors.</p>
 
-            Before joining PwC, Brandon was Executive VP at Surbana Jurong, a Temasek-owned urban planning and
+        <p>Before joining PwC, Brandon was Executive VP at Surbana Jurong, a Temasek-owned urban planning and
             engineering consulting firm, where he led M&A strategy and completed successful deals in China, Singapore,
             and Australia. He also served as CEO of an asset management company tied to a European family office,
             overseeing equity and debt financing with international institutions like the Asia Development Bank (ADB).
-            <br><br>
+        </p>
 
         <h4 class="h4">Professional Qualifications:</h4>
         <ul>
-            <li>Executive Master of Business Administration (EMBA), Nanyang Business School, NTU, Singapore</li>
-            <li>Bachelor of Engineering (Hons) from Nanyang Technological University, Singapore</li>
-            <li>Advanced Management Program at Hass School of Business, University of California Berkley, USA</li>
-            <li>Senior Executive Management Program at Tsinghua University, China</li>
-            <li>Strategic Management Program, NUS Business School, Singapore</li>
+            <li>
+                Executive Master of Business Administration (EMBA), Nanyang Business School, NTU, Singapore
+
+            </li>
+            <li>
+                Bachelor of Engineering (Honours) from Nanyang Technological University, Singapore
+
+            </li>
+            <li>
+                Advanced Management Program at Haas School of Business, University of California Berkley, USA
+
+            </li>
+            <li>
+                Senior Executive Management Program at Tsinghua University, China
+
+            </li>
+            <li>
+                Strategic Management Program, NUS Business School, Singapore
+
+            </li>
         </ul>
+
+        <!--About Ms. Genevieve Tan-->
+        <h3 class="h3 mt-5">Ms. Genevieve Tan</h3>
+        <p>Genevieve has a strong background in both corporate operations and education management. In 2015, she
+            spearheaded the successful relocation of the company to a custom-built 5-storey facility, overseeing the
+            integration of advanced technologies such as ERP, WMS, and specialized software to optimize operational
+            efficiency. She also oversees the branding and marketing aspect of the business.
         </p>
 
+
+        <p>Genevieve played a pivotal role in achieving ISO 9001 and ISO 22000 certifications by designing and
+            implementing the necessary systems and processes. Additionally, she led strategic partnerships with
+            organizations such as SATS and Geodis Logistics and many overseas suppliers across the world including
+            Southeast Asia, South Africa, Mexico, Italy, Spain, Australia and New Zealand.
+        </p>
+        <p>As the primary contact for audit and governance matters, she ensured compliance with industry standards,
+            while managing all aspects of company operations, including staff, financial oversight, facilities, and
+            security.
+        </p>
+        <p>Genevieve&#39;s experience in managing teams, implementing strategic initiatives, and mentoring individuals
+            across various sectors showcases her ability to lead diverse teams and deliver impactful results.
+        </p>
+        <h4 class="h4">Professional Qualifications:</h4>
+        <ul>
+            <li>Master of Science in Applied Economics, Singapore Management University
+            </li>
+            <li>Postgraduate Diploma in Education (Secondary), National Institute of Education Singapore
+            </li>
+            <li>Bachelor of Social Sciences (Honours in Economics), National University of Singapore
+            </li>
+        </ul>
     </div>
 @endsection
